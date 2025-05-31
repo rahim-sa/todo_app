3a4693f4ae3fc94f6d3f4ca371e145c4b5e9f87c
rahim-sa <sadighi.rahim@gmail.com>
2025-05-31 19:47:23 +0330

    The file name has been changed to todo_app_v1.py

---
+++ VERSION_1/todo_app_v1.py
@@ -0,0 +1,45 @@
+import time
+import json
+from datetime import date
+from datetime import datetime
+
+
+class Todo:
+    def __init__(self, title: str, description: str, due_date: date):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.is_complete = False
+        self.created_at = datetime.now()
+
+class TodoApp:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.todos = {}
+
+
+#### Required Functions
+#1. TODO Management
+ #  - add_todo(title, description, due_date)
+  # - complete_todo(title)
+   #- delete_todo(title)
+   #- list_todos()
+
+#2. File Operations
+ #  - save_to_file()
+  # - load_from_file()
+
+#### Success Criteria
+#- [ ] Basic CRUD operations working
+#- [ ] Data persists between sessions
+#- [ ] Basic error handling implemented
+#- [ ] Input validation working
+
+def main():
+    print("Hello from todo-app-v1!")
+
+
+if __name__ == "__main__":
+    main()
+
+print("base feature is on going")
