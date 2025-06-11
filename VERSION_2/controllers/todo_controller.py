0cde4e609542e4be2ff9514b9d85d32c011df7ac
rahim-sa <sadighi.rahim@gmail.com>
2025-06-11 08:57:11 +0330

    Feature: Status filtering and statistics

--- VERSION_2/controllers/todo_controller.py
+++ VERSION_2/controllers/todo_controller.py
@@ -9,3 +9,11 @@ class TodoController:
             choice = self.view.get_input("Choose option: ")
             if choice == '1':
                 self._handle_add_todo()
+
+    def _handle_list_todos(self):
+        filter_choice = input("Show [A]ll/[C]ompleted/[P]ending: ").upper()
+        todos = (
+            self.model.completed_todos if filter_choice == 'C' else
+            self.model.pending_todos if filter_choice == 'P' else
+            self.model.all_todos
+        )
--- VERSION_2/models/todo.py
+++ VERSION_2/models/todo.py
@@ -5,3 +5,7 @@ class Todo:
     due_date: date
     is_complete: bool = False
     created_at: datetime = field(default_factory=datetime.now)
+
+    @property
+    def is_overdue(self) -> bool:
+        return self.due_date < date.today() and not self.is_complete
