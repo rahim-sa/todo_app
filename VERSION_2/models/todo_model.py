36aee43cfdbc86baba4e102fd249f0e39d0a1487
rahim-sa <sadighi.rahim@gmail.com>
2025-06-12 08:54:18 +0330

    Feature: Search functionality
    - Title search
    - Description search
    - Date range search
    - Combined filters

--- VERSION_2/models/todo_model.py
+++ VERSION_2/models/todo_model.py
@@ -3,3 +3,13 @@ class TodoModel:
         self.file_path = file_path
         self.todos: Dict[str, Todo] = {}
         self.load_todos()
+
+    @property
+    def pending_todos(self) -> List[Todo]:
+        return [t for t in self.todos.values() if not t.is_complete]
+    
+
+def search(self, keyword: str) -> List[Todo]:
+    return [t for t in self.todos.values() 
+            if keyword.lower() in t.title.lower() 
+            or keyword.lower() in t.description.lower()]
